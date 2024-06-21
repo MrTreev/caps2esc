36e928f273af785dc60e1b0529b36fee8fff4978
MrTreev <mr.treev@gmail.com>
2024-06-21 11:10:49 +1000

    my changes

---
+++ Makefile
@@ -0,0 +1,4 @@
+CC		=	gcc
+CFLAGS	=	-I"/usr/include/libevdev-1.0" -levdev -ludev
+
+caps2esc: caps2esc.c
--- caps2esc.c
+++ caps2esc.c
@@ -3,46 +3,42 @@
 // run:
 //     sudo nice -n -20 ./caps2esc >caps2esc.log 2>caps2esc.err &
 
-#include <stdio.h>
 #include <errno.h>
-#include <string.h>
-#include <stdlib.h>
-
 #include <fcntl.h>
-#include <signal.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-
-#include <libudev.h>
-#include <sys/select.h>
-
-#include <libevdev/libevdev.h>
 #include <libevdev/libevdev-uinput.h>
+#include <libevdev/libevdev.h>
+#include <libudev.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-// clang-format off
 const struct input_event
-esc_up          = {.type = EV_KEY, .code = KEY_ESC,      .value = 0},
-ctrl_up         = {.type = EV_KEY, .code = KEY_LEFTCTRL, .value = 0},
-capslock_up     = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 0},
-esc_down        = {.type = EV_KEY, .code = KEY_ESC,      .value = 1},
-ctrl_down       = {.type = EV_KEY, .code = KEY_LEFTCTRL, .value = 1},
-capslock_down   = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 1},
-esc_repeat      = {.type = EV_KEY, .code = KEY_ESC,      .value = 2},
-ctrl_repeat     = {.type = EV_KEY, .code = KEY_LEFTCTRL, .value = 2},
-capslock_repeat = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 2};
-// clang-format on
+    capslock_up     = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 0},
+    capslock_repeat = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 2},
+    capslock_down   = {.type = EV_KEY, .code = KEY_CAPSLOCK, .value = 1},
+    ctrl_up         = {.type = EV_KEY, .code = KEY_LEFTMETA, .value = 0},
+    ctrl_repeat     = {.type = EV_KEY, .code = KEY_LEFTMETA, .value = 2},
+    ctrl_down       = {.type = EV_KEY, .code = KEY_LEFTMETA, .value = 1},
+    esc_up          = {.type = EV_KEY, .code = KEY_ESC, .value = 0},
+    esc_repeat      = {.type = EV_KEY, .code = KEY_ESC, .value = 2},
+    esc_down        = {.type = EV_KEY, .code = KEY_ESC, .value = 1};
 
-int equal(const struct input_event *first, const struct input_event *second) {
-    return first->type == second->type && first->code == second->code &&
-           first->value == second->value;
+int equal(const struct input_event* first, const struct input_event* second) {
+    return first->type == second->type && first->code == second->code
+           && first->value == second->value;
 }
 
-int eventmap(const struct input_event *input, struct input_event output[]) {
+int eventmap(const struct input_event* input, struct input_event output[]) {
     static int capslock_is_down = 0, esc_give_up = 0;
 
-    if (input->type == EV_MSC && input->code == MSC_SCAN)
+    if (input->type == EV_MSC && input->code == MSC_SCAN) {
         return 0;
+    }
 
     if (input->type != EV_KEY) {
         output[0] = *input;
@@ -50,8 +46,8 @@ int eventmap(const struct input_event *input, struct input_event output[]) {
     }
 
     if (capslock_is_down) {
-        if (equal(input, &capslock_down) || equal(input, &capslock_repeat) ||
-            input->code == KEY_LEFTCTRL) {
+        if (equal(input, &capslock_down) || equal(input, &capslock_repeat)
+            || input->code == KEY_LEFTMETA) {
             return 0;
         }
         if (equal(input, &capslock_up)) {
@@ -75,8 +71,9 @@ int eventmap(const struct input_event *input, struct input_event output[]) {
 
         output[k++] = *input;
 
-        if (output[k - 1].code == KEY_ESC)
+        if (output[k - 1].code == KEY_ESC) {
             output[k - 1].code = KEY_CAPSLOCK;
+        }
 
         return k;
     }
@@ -88,66 +85,92 @@ int eventmap(const struct input_event *input, struct input_event output[]) {
 
     output[0] = *input;
 
-    if (output[0].code == KEY_ESC)
+    if (output[0].code == KEY_ESC) {
         output[0].code = KEY_CAPSLOCK;
+    }
 
     return 1;
 }
 
-int eventmap_loop(const char *devnode) {
+int eventmap_loop(const char* devnode) {
     int result = 0;
     int fd     = open(devnode, O_RDONLY);
-    if (fd < 0)
+    if (fd < 0) {
         return 0;
+    }
 
-    struct libevdev *dev;
-    if (libevdev_new_from_fd(fd, &dev) < 0)
+    struct libevdev* dev;
+    if (libevdev_new_from_fd(fd, &dev) < 0) {
         goto teardown_fd;
+    }
 
     sleep(1);
 
-    if (libevdev_grab(dev, LIBEVDEV_GRAB) < 0)
+    if (libevdev_grab(dev, LIBEVDEV_GRAB) < 0) {
         goto teardown_dev;
-    if (libevdev_enable_event_type(dev, EV_KEY) < 0)
+    }
+    if (libevdev_enable_event_type(dev, EV_KEY) < 0) {
         goto teardown_grab;
-    if (libevdev_enable_event_code(dev, EV_KEY, KEY_ESC, NULL) < 0)
+    }
+    if (libevdev_enable_event_code(dev, EV_KEY, KEY_ESC, NULL) < 0) {
         goto teardown_grab;
-    if (libevdev_enable_event_code(dev, EV_KEY, KEY_CAPSLOCK, NULL) < 0)
+    }
+    if (libevdev_enable_event_code(dev, EV_KEY, KEY_CAPSLOCK, NULL) < 0) {
         goto teardown_grab;
-    if (libevdev_enable_event_code(dev, EV_KEY, KEY_LEFTCTRL, NULL) < 0)
+    }
+    if (libevdev_enable_event_code(dev, EV_KEY, KEY_LEFTMETA, NULL) < 0) {
         goto teardown_grab;
-    if (libevdev_disable_event_code(dev, EV_KEY, KEY_WLAN) < 0)
+    }
+    if (libevdev_disable_event_code(dev, EV_KEY, KEY_WLAN) < 0) {
         goto teardown_grab;
+    }
 
-    struct libevdev_uinput *udev;
-    if (libevdev_uinput_create_from_device(dev, LIBEVDEV_UINPUT_OPEN_MANAGED,
-                                           &udev) < 0)
+    struct libevdev_uinput* udev;
+    if (libevdev_uinput_create_from_device(
+            dev,
+            LIBEVDEV_UINPUT_OPEN_MANAGED,
+            &udev
+        )
+        < 0) {
         goto teardown_grab;
+    }
 
     for (;;) {
         struct input_event input;
-        int rc = libevdev_next_event(
-            dev, LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING,
-            &input);
+        int                rc = libevdev_next_event(
+            dev,
+            LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING,
+            &input
+        );
 
-        while (rc == LIBEVDEV_READ_STATUS_SYNC)
+        while (rc == LIBEVDEV_READ_STATUS_SYNC) {
             rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_SYNC, &input);
+        }
 
-        if (rc == -EAGAIN)
+        if (rc == -EAGAIN) {
             continue;
+        }
 
-        if (rc != LIBEVDEV_READ_STATUS_SUCCESS)
+        if (rc != LIBEVDEV_READ_STATUS_SUCCESS) {
             break;
+        }
 
         struct input_event output[2];
         for (int i = 0, k = eventmap(&input, output); i != k; ++i) {
             if (libevdev_uinput_write_event(
-                    udev, output[i].type, output[i].code, output[i].value) < 0)
+                    udev,
+                    output[i].type,
+                    output[i].code,
+                    output[i].value
+                )
+                < 0) {
                 goto teardown_udev;
+            }
             if (i + 1 != k) {
-                if (libevdev_uinput_write_event(udev, EV_SYN, SYN_REPORT, 0) <
-                    0)
+                if (libevdev_uinput_write_event(udev, EV_SYN, SYN_REPORT, 0)
+                    < 0) {
                     goto teardown_udev;
+                }
                 usleep(20000);
             }
         }
@@ -167,40 +190,60 @@ teardown_fd:
     return result;
 }
 
-void eventmap_exec(const char *self_path, const char *devnode) {
+void eventmap_exec(const char* self_path, const char* devnode) {
     switch (fork()) {
-        case -1:
-            fprintf(stderr, "Fork failed on %s %s (%s)\n", self_path, devnode,
-                    strerror(errno));
-            break;
-        case 0: {
-            char *command[] = {(char *)self_path, (char *)devnode, NULL};
+    case -1:
+        fprintf(
+            stderr,
+            "Fork failed on %s %s (%s)\n",
+            self_path,
+            devnode,
+            strerror(errno)
+        );
+        break;
+    case 0:
+        {
+            char* command[] = {(char*)self_path, (char*)devnode, NULL};
             execvp(command[0], command);
-            fprintf(stderr, "Exec failed on %s %s (%s)\n", self_path, devnode,
-                    strerror(errno));
-        } break;
+            fprintf(
+                stderr,
+                "Exec failed on %s %s (%s)\n",
+                self_path,
+                devnode,
+                strerror(errno)
+            );
+        }
+        break;
     }
 }
 
-int should_grab(struct udev_device *device, int initial_scan) {
-    if (device == NULL)
+int should_grab(struct udev_device* device, int initial_scan) {
+    if (device == NULL) {
         return 0;
-
-    const char virtual_devices_directory[] = "/sys/devices/virtual/input/";
-    if (strncmp(udev_device_get_syspath(device), virtual_devices_directory,
-                sizeof(virtual_devices_directory) - 1) == 0)
-        return 0;
-
-    if (!initial_scan) {
-        const char *action = udev_device_get_action(device);
-        if (!action || strcmp(action, "add"))
-            return 0;
     }
 
-    const char input_prefix[] = "/dev/input/event";
-    const char *devnode       = udev_device_get_devnode(device);
-    if (!devnode || strncmp(devnode, input_prefix, sizeof(input_prefix) - 1))
+    const char virtual_devices_directory[] = "/sys/devices/virtual/input/";
+    if (strncmp(
+            udev_device_get_syspath(device),
+            virtual_devices_directory,
+            sizeof(virtual_devices_directory) - 1
+        )
+        == 0) {
         return 0;
+    }
+
+    if (!initial_scan) {
+        const char* action = udev_device_get_action(device);
+        if (!action || strcmp(action, "add")) {
+            return 0;
+        }
+    }
+
+    const char  input_prefix[] = "/dev/input/event";
+    const char* devnode        = udev_device_get_devnode(device);
+    if (!devnode || strncmp(devnode, input_prefix, sizeof(input_prefix) - 1)) {
+        return 0;
+    }
 
     int fd = open(devnode, O_RDONLY);
     if (fd < 0) {
@@ -208,18 +251,22 @@ int should_grab(struct udev_device *device, int initial_scan) {
         return 0;
     }
 
-    struct libevdev *dev;
+    struct libevdev* dev;
     if (libevdev_new_from_fd(fd, &dev) < 0) {
-        fprintf(stderr, "Failed to create evdev device from %s (%s)\n", devnode,
-                strerror(errno));
+        fprintf(
+            stderr,
+            "Failed to create evdev device from %s (%s)\n",
+            devnode,
+            strerror(errno)
+        );
         close(fd);
         return 0;
     }
 
     int should_be_grabbed =
-        libevdev_has_event_type(dev, EV_KEY) &&
-        (libevdev_has_event_code(dev, EV_KEY, KEY_ESC) ||
-         libevdev_has_event_code(dev, EV_KEY, KEY_CAPSLOCK));
+        libevdev_has_event_type(dev, EV_KEY)
+        && (libevdev_has_event_code(dev, EV_KEY, KEY_ESC)
+            || libevdev_has_event_code(dev, EV_KEY, KEY_CAPSLOCK));
 
     libevdev_free(dev);
     close(fd);
@@ -233,7 +280,7 @@ void kill_zombies(__attribute__((unused)) int signum) {
         ;
 }
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char* argv[]) {
     int initial_scan;
 
     if (argc > 2) {
@@ -241,8 +288,9 @@ int main(int argc, const char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    if (argc == 2)
+    if (argc == 2) {
         return !eventmap_loop(argv[1]);
+    }
 
     struct sigaction sa;
     memset(&sa, 0, sizeof sa);
@@ -253,29 +301,32 @@ int main(int argc, const char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    struct udev *udev = udev_new();
+    struct udev* udev = udev_new();
     if (!udev) {
         perror("Can't create udev");
         return EXIT_FAILURE;
     }
 
-    struct udev_enumerate *enumerate = udev_enumerate_new(udev);
+    struct udev_enumerate* enumerate = udev_enumerate_new(udev);
     udev_enumerate_add_match_subsystem(enumerate, "input");
     udev_enumerate_scan_devices(enumerate);
-    struct udev_list_entry *devices = udev_enumerate_get_list_entry(enumerate);
-    struct udev_list_entry *dev_list_entry;
+    struct udev_list_entry* devices = udev_enumerate_get_list_entry(enumerate);
+    struct udev_list_entry* dev_list_entry;
     udev_list_entry_foreach(dev_list_entry, devices) {
-        struct udev_device *device = udev_device_new_from_syspath(
-            udev, udev_list_entry_get_name(dev_list_entry));
+        struct udev_device* device = udev_device_new_from_syspath(
+            udev,
+            udev_list_entry_get_name(dev_list_entry)
+        );
         if (device) {
-            if (should_grab(device, initial_scan = 1))
+            if (should_grab(device, initial_scan = 1)) {
                 eventmap_exec(argv[0], udev_device_get_devnode(device));
+            }
             udev_device_unref(device);
         }
     }
     udev_enumerate_unref(enumerate);
 
-    struct udev_monitor *monitor = udev_monitor_new_from_netlink(udev, "udev");
+    struct udev_monitor* monitor = udev_monitor_new_from_netlink(udev, "udev");
     if (!monitor) {
         perror("Can't create monitor");
         return EXIT_FAILURE;
@@ -290,10 +341,11 @@ int main(int argc, const char *argv[]) {
         FD_SET(fd, &fds);
 
         if (select(fd + 1, &fds, NULL, NULL, NULL) > 0 && FD_ISSET(fd, &fds)) {
-            struct udev_device *device = udev_monitor_receive_device(monitor);
+            struct udev_device* device = udev_monitor_receive_device(monitor);
             if (device) {
-                if (should_grab(device, initial_scan = 0))
+                if (should_grab(device, initial_scan = 0)) {
                     eventmap_exec(argv[0], udev_device_get_devnode(device));
+                }
                 udev_device_unref(device);
             }
         }
